83633ae3759c2e20b3a3779dfa08122fd4462c31
prabhat pandey <prabhat@battimuni.localhost>
2020-10-21 06:10:07 +0545

    updated alias to include git

--- shell/alias.zsh
+++ shell/alias.zsh
@@ -1,7 +1,21 @@
 # zsh alias
 alias greet="echo hello"
+# basic command
 alias q="exit"
-alias src="source ~/.zshrc"
 alias la="ls -a"
 alias lla="ls -la"
 alias vim="nvim"
+# zshrc
+alias src="source ~/.zshrc"
+# git alias
+alias gst="git status"
+alias ga="git add "
+alias gaa="git add ."
+alias gcmsg="git commit -m"
+alias gcmsga="git commit -am"
+alias gpsh="git push"
+alias gpl="git pull"
+alias gfh="git fetch"
+alias gco="git checkout"
+alias glg='git log --stat --max-count=5'
+alias glgo='git log --oneline'
